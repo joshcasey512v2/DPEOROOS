be09ec1401e5ebc3c40813bfc572233e45b13d48
unknown <josh.casey24@mail.dcu.ie>
2025-05-27 18:53:20 +0100

    Adding new exercise: oop.cpp

---
+++ exercises/oop.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+// Define a class named MyClass
+class MyClass
+{
+private:
+    // Private member variable to store the name
+    std::string name;
+
+public:
+    // Constructor declaration: called when an object is created
+    MyClass(std::string n);
+
+    // Destructor declaration: called when an object is destroyed
+    ~MyClass();
+};
+
+// Constructor definition
+// This initializes the 'name' member using an initializer list
+// and prints a message indicating that the object has been created.
+MyClass::MyClass(std::string n) : name(n)
+{
+    std::cout << "Constructor => MyClass object for \"" << name << "\" created." << std::endl;
+}
+
+// Destructor definition
+// This is automatically called when the object goes out of scope.
+// It prints a message indicating that the object is being destroyed.
+MyClass::~MyClass()
+{
+    std::cout << "Destructor => MyClass object for \"" << name << "\" destroyed." << std::endl;
+}
+
+// Main function to demonstrate the use of constructor and destructor
+int main()
+{
+    // Create an instance of MyClass.
+    // TODO: Initiate the class and constructor with a message of your type
+    // MyClass obj();
+
+    // When main() ends and 'obj' goes out of scope,
+    // the destructor is automatically called.
+    return 0;
+}
